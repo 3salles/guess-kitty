dab34d7e419cdc0e0c64e6aaf368a14dd4e66257
Beatriz Salles <beatrizsallesss@gmail.com>
2022-01-25 12:38:09 -0300

    Create header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
-import { Flex, Heading, Text } from "@chakra-ui/react"
+import { Flex, Heading, Image, Text } from "@chakra-ui/react";
 
 export const Header = () => {
   return (
-    <Heading>
+    <Heading bg="purple.200" p="2">
       <Flex align="center" justify="center">
-        <Text>Gatinho!</Text>
+        <Image
+          src="https://img.icons8.com/external-tulpahn-flat-tulpahn/64/000000/external-happy-cat-emoji-tulpahn-flat-tulpahn.png"
+          alt="Gatinho"
+        />
+        <Text ml="4">Gatinho!</Text>
       </Flex>
     </Heading>
-  )
-}
+  );
+};
