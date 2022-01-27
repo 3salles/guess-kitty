8b64f2c20704be76195ddd1c3b7747933f0f7e3d
Beatriz Salles <beatrizsallesss@gmail.com>
2022-01-27 18:03:51 -0300

    chore: Craete confirm result page

--- src/components/pages/home/components/confirm/index.tsx
+++ src/components/pages/home/components/confirm/index.tsx
@@ -1,9 +1,31 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Button, Center, Heading, Image, Text, VStack } from "@chakra-ui/react";
 
-export const ConfirmSection = () => {
-  return (
-    <Box as="article">
-      Kitty
-    </Box>
-  )
+interface ConfirmSectionProps {
+  setResultChecked: (status: boolean) => void;
 }
+
+export const ConfirmSection = ({ setResultChecked }: ConfirmSectionProps) => {
+  return (
+    <Center h="full" as="section" p="8">
+      <VStack spacing="20">
+        <Heading as="header" textAlign="center" fontSize="2xl">
+          É um gatinho!!
+        </Heading>
+        <Box h="full" as="article" mt="4" fontSize="2xl">
+          <Image
+            src="https://acegif.com/wp-content/gifs/dancing-cat-25.gif"
+            alt="Gatinho feliz"
+          />
+          <Text mt="2" fontSize="lg">Tenho 84% de certeza</Text>
+        </Box>
+        <Button
+          colorScheme="purple"
+          color="white"
+          onClick={() => setResultChecked(false)}
+        >
+          Tentar novamente
+        </Button>
+      </VStack>
+    </Center>
+  );
+};
--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,10 +1,20 @@
+import { useDisclosure } from "@chakra-ui/react";
+import { useState } from "react";
+import { Drawer } from "../../common/Drawer";
+import { ConfirmSection } from "./components/confirm";
 import { MainContent } from "./components/MainContent";
 
 export const Home = () => {
+  const isCat = true;
+  const [resultChecked, setResultChecked] = useState(true);
 
   return (
     <>
-      <MainContent />
+      {resultChecked ? (
+        <ConfirmSection setResultChecked={setResultChecked} />
+      ) : (
+        <MainContent />
+      )}
     </>
   );
 };
