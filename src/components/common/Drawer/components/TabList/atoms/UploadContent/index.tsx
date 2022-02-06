494668e0c7d0f182286639fae4dd792c907d34bd
Beatriz Salles <beatrizsallesss@gmail.com>
2022-02-06 12:36:20 -0300

    chore: Convert images to base64

--- src/components/common/Drawer/components/TabList/atoms/UploadContent/index.tsx
+++ src/components/common/Drawer/components/TabList/atoms/UploadContent/index.tsx
@@ -1,55 +1,40 @@
 import { Center, Image, Text } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useDropzone } from "react-dropzone";
 
-
-interface ImageFile extends Blob {
-  preview: string;
-}
-
 export const UploadContent = () => {
-  const [file, setFile] = useState<ImageFile>();
+  const [file, setFile] = useState<any>();
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     accept: "image/*",
     onDrop: (acceptedFiles) => {
+      const base64 = convertToBase64(acceptedFiles[0]);
       setFile(
         Object.assign(acceptedFiles[0], {
-          preview: URL.createObjectURL(acceptedFiles[0]),
+          base64: base64,
         })
       );
     },
   });
 
-  // const getBase64 = async (file: ImageFile | undefined): Promise<string | undefined> => {
-  //   var reader = new FileReader();
-  //   reader.readAsDataURL(file as Blob);
-  
-  //   return new Promise((reslove, reject) => {
-  //     reader.onload = () => reslove(reader.result as any);
-  //     reader.onerror = (error) => reject(error);
-  //   })
-  // }
-
-  console.log(file)
-
-  useEffect(
-    () => () => {
-      // Make sure to revoke the data uris to avoid memory leaks
-      if (file) {
-        URL.revokeObjectURL(file?.preview);
-      }
-    },
-    [file]
-  );
-
-  console.log(file);
+  const convertToBase64 = (file: any) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+      fileReader.onload = () => {
+        setFile(fileReader.result);
+      };
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
 
   return (
     <>
       {file ? (
         <Center mt="8" border={"1px dashed white"}>
-          <Image src={file?.preview} alt="Imagem carregada" />
+          <Image src={file} alt="Imagem carregada" />
         </Center>
       ) : (
         <Center
